b9f26c7d1ce0aed4492e0d48f97f0f9c271a4555
Shuhei-pp <lotakotayota@gmail.com>
2022-05-14 23:27:06 +0900

    コンテスト出た

---
+++ 20220514_251/2-a.py
@@ -0,0 +1,16 @@
+n,w= map(int, input().split())
+A=list(map(int, input().split()))
+c_list=[]
+for i,item in enumerate(A):
+  if item <= w and not item in c_list:
+    c_list.append(item)
+for i in range(1,n):
+  for j in range(i+1,n+1):
+    if A[i-1]+A[j-1] <= w and not A[i-1]+A[j-1] in c_list:
+      c_list.append(A[i-1]+A[j-1])
+for i in range(1,n-1):
+  for j in range(i+1,n):
+    for k in range(j+1,n+1):
+      if A[i-1]+A[j-1]+A[k-1] <= w and not A[i-1]+A[j-1]+A[k-1] in c_list:
+        c_list.append(A[i-1]+A[j-1]+A[k-1])
+print(len(c_list))
