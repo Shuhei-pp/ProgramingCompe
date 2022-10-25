2ba0f3f7fdedfe5d07a67ca3515c921677505e27
Shuhei-pp <lotakotayota@gmail.com>
2022-10-25 09:52:02 +0900

    コミット

--- ABC/263/C.py
+++ ABC/263/C.py
@@ -1,17 +1,24 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-N,M =  map(int, input().split())
+# N,M =  map(int, input().split())
 
 
-def p(x,y,z,A):
-  for i in range(x,y+1):
-    B =A[:]
-    B.append(i)
-    if len(B)==z:
-      print(*B)
-      continue
-    p(i+1,M,z,B)
+# def p(x,y,z,A):
+#   for i in range(x,y+1):
+#     B =A[:]
+#     B.append(i)
+#     if len(B)==z:
+#       print(*B)
+#       continue
+#     p(i+1,M,z,B)
 
-A = []
-p(1,M,N,A)
+# A = []
+# p(1,M,N,A)
+
+from itertools import combinations
+
+n,m = map(int, input().split())
+
+for l in combinations(range(1, m + 1), n):
+    print(*l, sep=' ')
